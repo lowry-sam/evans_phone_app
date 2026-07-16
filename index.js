085a09a80513240ff5b7ad9f5387beac37e6492e
Sam Lowry <slowry44@me.com>
2026-07-16 19:18:32 -0400

    Update app name in AppRegistry registration

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App.js';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent("main", () => App);
